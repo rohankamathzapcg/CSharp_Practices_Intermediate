cfb8e1ccb3812b1b64c7d64d597db988f9e55c7d
rohankamathzapcg <rohan.kamath@zapcg.com>
2024-02-20 10:21:50 +0530

    Commit

--- Part-1/obj/Debug/net8.0/Part-1.AssemblyInfo.cs
+++ Part-1/obj/Debug/net8.0/Part-1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Part-1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa3201c42701d9c8b08be7b3d894b48f3fd9f314")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5e0017d5b8564873b4cade78983e2831aae1d00")]
 [assembly: System.Reflection.AssemblyProductAttribute("Part-1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Part-1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
