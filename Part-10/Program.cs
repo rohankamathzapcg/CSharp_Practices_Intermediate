b2748de07782f67019b22f411d8fcba43500c506
rohankamathzapcg <rohan.kamath@zapcg.com>
2024-02-20 15:34:26 +0530

    Afternoon Session-5

---
+++ Part-10/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Part10
+{
+    public class Circle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Draw a circle");
+        }
+    }
+
+    public class Rectangle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Draw a rectangle");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var circle =new Circle();
+            circle.Draw();
+
+            var rectangle=new Rectangle();
+            rectangle.Draw();
+
+        }
+    }
+}
---
+++ Part-10/Shape.cs
@@ -0,0 +1,16 @@
+namespace Part10
+{
+    public abstract class Shape
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public abstract void Draw();
+
+        public void Copy()
+        {
+            Console.WriteLine("Select the shape");
+            
+        }
+    }
+}
