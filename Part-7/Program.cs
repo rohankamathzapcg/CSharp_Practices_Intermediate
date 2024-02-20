4cff9fdddfa89e609128f277ccd72f7c6520c0b0
rohankamathzapcg <rohan.kamath@zapcg.com>
2024-02-20 13:03:42 +0530

    Afternoon Session-2

---
+++ Part-7/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace part7
+{
+    public class Vehicle
+    {
+        private readonly string _registrationNumber;
+        // public Vehicle()
+        // {
+        //     Console.WriteLine("Vehicle is being initialized");
+        // }
+
+        public Vehicle(string registrationNumber)
+        {
+            _registrationNumber = registrationNumber;
+            Console.WriteLine("Vehicle is being initialized");
+
+        }
+    }
+
+    public class Car : Vehicle
+    {
+        public Car(string registrationNumber) : base(registrationNumber)
+        {
+            Console.WriteLine("Car is being initialized.");
+        }
+    }
+
+    public class program
+    {
+        static void Main(string[] args)
+        {
+            var car = new Car("XYZ!1223");
+        }
+    }
+}
